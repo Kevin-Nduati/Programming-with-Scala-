9c46121832456a64706bb8a8b10142f9a05f62c9
kevin-nduati <crtskvn@gmail.com>
2022-07-07 00:04:13 +0300

    intro

---
+++ tutorial/src/main/scala/Main.scala
@@ -0,0 +1,15 @@
+@main def hello: Unit = 
+  println("Hello world!")
+  println("I am fine")
+  println(msg)
+  println("This is very interesting")
+  println(2 + 6)
+
+def msg = "I was compiled by Scala 3. :)"
+ 
+// object Main:
+//   val name = Option('Bob')
+//   name.get
+
+// println("Hello world!")
+
